eafeff3a655d85b76d258c23a6e798aabafc8205
Your Name <you@example.com>
2021-05-06 14:46:00 +0000

    Some fix

--- start_docker.sh
+++ start_docker.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+# Run Me
+# wget -O - https://raw.githubusercontent.com/zl0n/digi/main/start_docker.sh|bash
 apt update
-apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common mc git ncdu htop atop tmux
+apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common mc git ncdu htop atop tmux
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 apt update
-apt install docker-ce
+apt install -y docker-ce
 sudo curl -L https://github.com/docker/compose/releases/download/1.29.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
